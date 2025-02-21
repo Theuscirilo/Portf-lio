82200d297e954c84c3fae38ffd11d71f64a3474e
Matheus Cirilo de Almeida <matheuscirilodealmeida@gmail.com>
2025-02-21 11:23:07 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Portfolio from "./Portfolio";
+import "./index.css";
+
+ReactDOM.render(<Portfolio />, document.getElementById("root"));
 
